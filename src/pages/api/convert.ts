5a63627c33452ffac9bd0f9c6f29d5e7dd5afa2d
Jasmine <jasminetanjm@gmail.com>
2024-10-01 20:30:40 +0800

    add yt cookies

--- src/pages/api/convert.ts
+++ src/pages/api/convert.ts
@@ -1,5 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from "next";
 import ytdl from "@distube/ytdl-core";
+import * as fs from "fs";
 
 export default async function handler(
   req: NextApiRequest,
@@ -16,15 +17,17 @@ export default async function handler(
     return;
   }
   try {
-    const videoInfo = await ytdl.getBasicInfo(url);
+    const agent = ytdl.createAgent(JSON.parse(fs.readFileSync("src/utils/cookies.json", "utf-8")));
+    const videoInfo = await ytdl.getBasicInfo(url, {agent});
     console.log("videoInfo:", videoInfo);
     const title = videoInfo.videoDetails.title;
     const thumbnail = videoInfo.videoDetails.thumbnails[0].url;
     res.status(200).json({ title, thumbnail });
   } catch (error) {
-    res.status(500).json({ error: "Error converting video, please try again later",
-      description: error
-     });
-    console.log("error:", error)
+    res.status(500).json({
+      error: "Error converting video, please try again later",
+      description: error,
+    });
+    console.log("error:", error);
   }
 }
